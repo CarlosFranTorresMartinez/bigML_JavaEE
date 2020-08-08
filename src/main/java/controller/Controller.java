f0b5a732b1c31aba3637a6a60fcad86a7bd6adf7
Bryan.Benavente <bryan.benavente@vallegrande.edu.pe>
2020-08-08 00:58:22 -0500

    Ajustes de controlador

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -13,43 +13,23 @@ import services.Services;
 @ViewScoped
 public class Controller implements Serializable{
     
-    private final Services services = new Services();
-    private Model model = new Model();
+    private final Services services;
+    private Model model;
     
     public Controller() {
+        model = new Model();
+        services = new Services();
     }
     
-    public void mostarDatos(String json) throws IOException, InterruptedException {
+    public void obtenerDatos() throws IOException, InterruptedException {
+        String a = services.datosAPI(model);
         JsonParser jsonParser = new JsonParser();
-        JsonObject jsonObject = (JsonObject) jsonParser.parse(services.datosAPI(json));
-        
+        JsonObject jsonObject = (JsonObject) jsonParser.parse(a);        
         model.setConfidence(jsonObject.get("confidence").getAsDouble());
         System.out.println(jsonObject.get("confidence").getAsDouble());
         model.setCategory(jsonObject.get("category").getAsInt());
         model.setCredits(jsonObject.get("credits").getAsDouble());
         model.setProbability(jsonObject.get("probability").getAsDouble());
-    }
-    
-    public void obtenerDatos() throws IOException, InterruptedException {
-        String json = "{\n"
-                + "    \"model\": \"logisticregression/5f24ad002fb31c2967005efa\",\n"
-                + "    \"input_data\" : {\n"
-                + "        \"age\": " + model.getAge() + " ,\n"
-                + "        \"anaemia\": " + model.getAnaemia() + ",\n"
-                + "        \"creatinine_phosphokinase\": " + model.getCreatinine_phosphokinase() + ",\n"
-                + "        \"diabetes\": " + model.getDiabetes() + ",\n"
-                + "        \"ejection_fraction\": " + model.getEjection_fraction() + ",\n"
-                + "        \"high_blood_pressure\": " + model.getHigh_blood_pressure() + ",\n"
-                + "        \"platelets\": " + model.getPlatelets() + ",\n"
-                + "        \"serum_creatinine\": " + model.getSerum_creatinine() + ",\n"
-                + "        \"serum_sodium\": " + model.getSerum_sodium() + " ,\n"
-                + "        \"sex\": " + model.getSex() + ",\n"
-                + "        \"smoking\": " + model.getSmoking() + "\n"
-                + "    }\n"
-                + "}";
-        
-        String a = services.datosAPI(json);
-        mostarDatos(a);
     }
     
     public Model getModel() {
--- src/main/java/services/Services.java
+++ src/main/java/services/Services.java
@@ -5,14 +5,29 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import model.Model;
 
 public class Services {
 
-    public String datosAPI(String json) throws IOException, InterruptedException {
-
+    public String datosAPI(Model json) throws IOException, InterruptedException {
+        StringBuilder entity = new StringBuilder();
+        entity.append("{");
+        entity.append("\"age\":").append(json.getAge()).append(",");
+        entity.append("\"anaemia\":").append(json.getAnaemia()).append(",");
+        entity.append("\"creatinine_phosphokinase\":").append(json.getCreatinine_phosphokinase()).append(",");
+        entity.append("\"diabetes\":").append(json.getDiabetes()).append(",");
+        entity.append("\"ejection_fraction\":").append(json.getEjection_fraction()).append(",");
+        entity.append("\"high_blood_pressure\":").append(json.getHigh_blood_pressure()).append(",");
+        entity.append("\"platelets\":").append(json.getPlatelets()).append(",");
+        entity.append("\"serum_creatinine\":").append(json.getSerum_creatinine()).append(",");
+        entity.append("\"serum_sodium\":").append(json.getSerum_sodium()).append(",");
+        entity.append("\"sex\":").append(json.getSex()).append(",");
+        entity.append("\"smoking\":").append(json.getSmoking());
+        entity.append("}");
+        
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = (HttpRequest) HttpRequest.newBuilder()
-                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .POST(HttpRequest.BodyPublishers.ofString(entity.toString()))
                 .uri(URI.create("https://bigml.io/andromeda/prediction?username=bryanbenavente;api_key=6bdd6d1dbf8ac8a270e56896062c3083555bfcc8"))
                 .setHeader("User-Agent", "Java 11 HttpClient Bot")
                 .header("Content-Type", "application/json")
